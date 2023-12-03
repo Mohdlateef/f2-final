0a403c95f4852a9320cc600cacc998ac06419d73
unknown <lateefahbaba123@gmail.com>
2023-12-03 20:55:01 +0530

    final

--- script.js
+++ script.js
@@ -1,135 +1,77 @@
-let timers = [];
-let flag=0;
-function startNewTimer() {
-   
-    const hours = parseInt(document.getElementById('hours').value) || 0;
-    let minutes = parseInt(document.getElementById('minutes').value) || 0;
-    let seconds = parseInt(document.getElementById('seconds').value) || 0;
 
-    // Ensure minutes and seconds are capped at 60
-    minutes = Math.min(minutes, 60);
-    seconds = Math.min(seconds, 60);
+const setButton = document.getElementById("set-btn");
+const activeTimersContainer = document.getElementById(
+  "active-timers-container"
+);
 
-    // Adjust hours if needed
-    if (minutes >= 60) {
-        hours += Math.floor(minutes / 60);
-        minutes %= 60;
-    }
 
+const noTimerDisplayMessage = document.getElementById("no-timers-display-msg");
+const audio = document.getElementById("audio-file");
+let numberOfActiveTimers = 0;
+
+
+function addNewTimer() {
+    const hours = Number(document.getElementById("hour").value);
+    const minutes = Number(document.getElementById("minute").value);
+    const seconds = Number(document.getElementById("seconds").value);
+  
+    // total time calculation in seconds
     const totalTime = hours * 3600 + minutes * 60 + seconds;
-
+  
     if (totalTime > 0) {
-        const timerId = setInterval(() => {
-            const timerIndex = timers.findIndex(timer => timer.id === timerId);
-            if (timerIndex !== -1) {
-                if (timers[timerIndex].totalTime <= 0) {
-                    clearInterval(timerId);
-                    handleTimerEnd(timerId);
-                } else {
-                    timers[timerIndex].totalTime--;
-
-                    updateTimersDisplay();
-                }
-            }
-        }, 1000);
-
-        timers.push({ id: timerId, totalTime });
-        updateTimersDisplay();
-    } else {
-        alert('Please enter a valid time.');
+      const newTimerCard = document.createElement("div");
+      newTimerCard.classList.add("timer-card");
+      newTimerCard.innerHTML = `
+          <span>Time Left :</span>
+          <span id="timer-container">
+              <input id="hour" type="number" min="0" max="24" placeholder="${hours.toString().padStart(2, "0")}">:
+              <input id="minute" type="number" min="0" max="60" placeholder="${minutes.toString().padStart(2, "0")}">:
+              <input id="seconds" type="number" min="0" max="60" placeholder="${seconds.toString().padStart(2, "0")}">
+          </span>
+          <button class="btn" onclick="deleteTimer(this)">Delete</button>
+          `;
+      activeTimersContainer.appendChild(newTimerCard);
+      numberOfActiveTimers++;
+      noTimerDisplayMessage.style.display = "none";
+      runtimer(totalTime, newTimerCard);
     }
-}
-
-
-function handleTimerEnd(timerId) {
-    const timerIndex = timers.findIndex(timer => timer.id === timerId);
-    if (timerIndex !== 1) {
-        const audio = document.getElementById("music");
+  }
+  // funtion to run the timer which is added
+  function runtimer(totalTime, newTimerCard) {
+    let timerContainer = newTimerCard.querySelector("#timer-container");
+    let hour = timerContainer.querySelector("#hour");
+    let minute = timerContainer.querySelector("#minute");
+    let seconds = timerContainer.querySelector("#seconds");
+  
+    // now set time interval for each second to update the values in DOM
+    const myTimerUpdate = setInterval(() => {
+      if (totalTime == 0) {
+        newTimerCard.classList.toggle("time-up");
+        newTimerCard.innerHTML = `
+                      <span></span>
+                      <span>Timer is Up!</span>
+                      <button class="btn delete-btn" onclick="deleteTimer(this)">Stop</button>
+                  `;
         audio.play();
-       
-        audio.loop=true;
-        // flag++;
-
-        // timers.splice(timerIndex, 1);
-        // updateTimersDisplay();
+        clearInterval(myTimerUpdate);
+      } else {
+        --totalTime;
+        hour.value = Math.floor(totalTime / 3600);
+        minute.value = Math.floor((totalTime % 3600) / 60);
+        seconds.value = totalTime % 60;
+      }
+    }, 1000);
+  }
+  
+  // delete timer function
+  function deleteTimer(deleteButton) {
+    audio.pause();
+    let currentTimerCard = deleteButton.parentNode;
+    currentTimerCard.remove();
+    --numberOfActiveTimers;
+    if (numberOfActiveTimers == 0) {
+      noTimerDisplayMessage.style.display = "block";
     }
-}
-
-function updateTimersDisplay() {
-    const activeTimersContainer = document.getElementById('activeTimers');
-    activeTimersContainer.innerHTML = '';
-
-    timers.forEach(timer => {
-        const timerElement = document.createElement('div');
-        timerElement.classList.add('timer');
-        const timeRemaining = formatTime(timer.totalTime);
-        timerElement.innerHTML = `
-        <p id="left">Time Left</p>
-        <p id="up">Timer is up</p>
-            <span>${timeRemaining}</span>
-            <button id="stop" onclick="stopTimer() ">Delete </button>
-            <button id="stopmusic" onclick="stopMusic() ">Stop </button>
-            
-        `;
-        
-        if (timer.totalTime <= 0) {
-            timerElement.classList.add('timer-ended');
-            // console.log(timerElement.button);
-            // let lastbutton=document.getElementById("stop");
-            // console.log(timerElement[].innerText);
-            // console.log(lastbutton.innerText);
-            // let music=document.querySelector("stopmusic");
-        
-
-        }
-
-        
-
-        activeTimersContainer.appendChild(timerElement);
-    });
-
-}
-function stopTimer(timerId){
-
-  clearInterval(timerId);
-const timerIndex = timers.findIndex(timer => timer.id === timerId);
-    if (timerIndex !== 1) {
-        // const audio = document.getElementById("music");
-        // audio.play();
-
-        timers.splice(timerIndex, 1);
-        updateTimersDisplay();
-    }
-    // console.log("abc");
-}
-function stopMusic(timerId){
-    
-    
-    const timerIndex = timers.findIndex(timer => timer.id === timerId);
-    
-         const audio = document.getElementById("music");
-         audio.currentTime=0;
-        //  console.log("abc");
-        
-         timers.splice(timerIndex, 1);
-         updateTimersDisplay();
-    
-    
-        audio.pause();
-    
-
-    
-}
-// function stopTimer(timerId) {
-//     handleTimerEnd(timerId);
-// }
-
-function formatTime(totalSeconds) {
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-
-    return `${hours.toString().padStart(2, '0')}:
-            ${minutes.toString().padStart(2, '0')}:
-            ${seconds.toString().padStart(2, '0')}`;
-}
+  }
+  // event listeners
+  setButton.addEventListener("click", addNewTimer);
